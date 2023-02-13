893b56c7b12dacca430c384afb19a7a3a75c6bc1
unknown <bgw595@myy.haaga-helia.fi>
2023-02-13 20:37:05 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [result, setResult] = useState('');
+  const [x, setX] = useState('');
+  const [y, setY] = useState('');
+  const [history, setHistory] = useState([]);
+
+  const checkValues = () => {
+    let check = true;
+    if (isNaN(x) && !isNaN(y)) {
+      check = false;
+      setResult('Type the first value please')
+    } else if (!isNaN(x) && isNaN(y)) {
+      check = false;
+      setResult('Type the second value please')
+    } else if (isNaN(x) && isNaN(y)) {
+      check = false;
+      setResult('Type the values please')
+    }
+    return (check);
+  }
+
+  const makeMinus = () => {
+    let check = checkValues();
+    if (check) {
+      setResult(`Result: ${x - y}`);
+      setHistory([`${x} - ${y} = ${x - y}`, ...history]);
+    }
+  }
+
+  const makePlus = () => {
+    if (checkValues()) {
+      setResult(`Result: ${x + y}`);
+      setHistory([`${x} + ${y} = ${x + y}`, ...history]);
+    }
+  }
+
+  const clearAll = () => {
+    setResult('');
+    setX('');
+    setY('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{result}</Text>
+      <TextInput
+        style={styles.textfield}
+        onChangeText={currValue => setX(parseInt(currValue))}
+        value={x}
+        keyboardType='numeric'
+      />
+      <TextInput
+        keyboardType='numeric'
+        style={styles.textfield}
+        onChangeText={currValue => setY(parseInt(currValue))}
+        value={y}
+      />
+      <View style={{ flexDirection: 'row' }}>
+        <Button onPress={makePlus} title='+' />
+        <Button onPress={makeMinus} title='-' />
+      </View>
+      <Button onPress={clearAll} title='Clear' />
+      <Text>History:</Text>
+      <FlatList
+        data={history}
+        renderItem={({ item }) => <Text style={{ fontSize: 18 }}>{item}</Text>}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 80,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textfield:
+  {
+    height: 30,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
